e2765841b6ddfd1141b464d829cedb2e20259683
Natã Alexandre Da Silva <natatgh@gmail.com>
2024-06-06 12:38:27 -0300

    Ended

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
-import os
 from flask import Flask, request, jsonify
 from flask_graphql import GraphQLView
 from schema import schema
 from database import db_session, init_db
 from auth import authenticate_user, validate_api_key
-
-# Certifique-se de que o diretório /tmp exista
-if not os.path.exists("/tmp"):
-    os.makedirs("/tmp")
 
 app = Flask(__name__)
 
@@ -27,7 +22,8 @@ def handle_error(e):
 
 class AuthGraphQLView(GraphQLView):
     def dispatch_request(self):
-        api_key = request.headers.get('X-API-KEY')
+        # Verificar chave de API no cabeçalho e no parâmetro de consulta
+        api_key = request.headers.get('X-API-KEY') or request.args.get('x-api-key')
         
         if not api_key or not validate_api_key(api_key):
             return jsonify({"message": "Invalid API Key"}), 403
